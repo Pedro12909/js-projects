accc86299baa5581951ecaf3f0d8340283c1aed6
Pedro12909 <pedro1290@hotmail.com>
2020-11-01 17:39:57 +0000

    [todo-app] persistence via local storage

--- todo-app/script.js
+++ todo-app/script.js
@@ -1,11 +1,46 @@
 const todoList = document.getElementById('todoList');
 const inputBox = document.getElementById('addInput');
 
-function deleteItemHandler(event) {
+const LOCAL_STORAGE_KEY = 'todo_app_item_list';
+
+// Get items from local storage
+let items = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [];
+
+items.forEach(item => {
+    todoList.appendChild(createListItemElement(item));
+});
+
+function checkItemHandler(li) {
+    li.classList.toggle('checked')
+}
+
+function addItemHandler(event) {
+    if (event.keyCode == 13) {
+        const inputBoxValue = inputBox.value.trim();
+
+        if (inputBoxValue === '') return;
+
+        // Append todoElement
+        todoList.appendChild(createListItemElement(inputBoxValue));
+        
+        // Persist in local storage
+        items.push(inputBoxValue);
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(items));
+
+        // Reset input box value
+        inputBox.value = '';
+    }
+}
+
+function deleteItemHandler(event, name) {
     const btn = event.target;
 
     const li = btn.parentNode.parentNode;
     const ul = li.parentNode;
+
+    // Persist in local storage
+    items = items.filter(item => item !== name)
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(items));
 
     ul.removeChild(li);
 }
@@ -14,46 +49,29 @@ function createListItemElement(name) {
     const li = document.createElement('li');
      
     const row = document.createElement('div');
-    row.classList.add('container');
 
     // Check Button
     const checkBtn = document.createElement('button');
     checkBtn.type = 'button';
     checkBtn.textContent = 'Check';
-    checkBtn.classList.add('checkBtn');
+    checkBtn.addEventListener('click', () => checkItemHandler(li));
     row.appendChild(checkBtn);
 
     // Text
     const text = document.createElement('p');
     text.innerText = name;
-    text.classList.add('todoText');
     row.appendChild(text);
 
     // Remove Button
     const delBtn = document.createElement('button');
     delBtn.type = 'button';
     delBtn.textContent = 'Delete';
-    delBtn.classList.add('delBtn');
-    delBtn.addEventListener('click', evt => deleteItemHandler(evt))
+    delBtn.addEventListener('click', evt => deleteItemHandler(evt, name));
     row.appendChild(delBtn);
 
     li.appendChild(row);
     return li;
 }
 
-function addTodoEventHandler(event) {
-    if (event.keyCode == 13) {
-        const inputBoxValue = inputBox.value.trim();
-
-        if (inputBoxValue === '') return;
-
-        // Append todoElement
-        todoList.appendChild(createListItemElement(inputBoxValue));
-
-        // Reset input box value
-        inputBox.value = '';
-    }
-}
-
-inputBox.addEventListener('keydown', e => addTodoEventHandler(e))
+inputBox.addEventListener('keydown', e => addItemHandler(e))
 
